82458f439a213060ec7128a06e28871dc5b41592
Lucas <lucaspesilva@gmail.com>
2021-01-13 22:58:43 -0400

    - Configurando Swagger UI para receber um token JWT

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # artistas-e-albuns-api
 Projeto prático de backend do processo seletivo da Polícia Judiciária Civil de MT
+
+Recuperar token JWT: curl.exe -i -X POST "http://localhost:8080/login" -d "{ \"username\":\"user\", \"password\":\"123\" }" -H "Content-Type: application/json"
--- src/main/java/br/com/dekagames/artistasealbunsapi/SwaggerConfig.java
+++ src/main/java/br/com/dekagames/artistasealbunsapi/SwaggerConfig.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -18,14 +21,27 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.dekagames.artistasealbunsapi.controller"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build()
-                .securitySchemes(Arrays.asList(new ApiKey("Bearer", "Authorization", "header")))
-                .securityContexts(Arrays.asList(SecurityContext.builder()
-                        .forPaths(PathSelectors.ant("/*"))
-                        .build()));
+                .build();
 
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 }
